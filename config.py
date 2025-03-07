34d67ca2873003629a166b83f2d6601a249b5f7f
faizan813 <f3983687@gmail.com>
2025-03-07 23:51:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ APP_ID = int(os.environ.get("APP_ID", "22805894"))
 API_HASH = os.environ.get("API_HASH", "4252e0c226a61540c4fb90d71fbd8f99")
 
 # Generate a user session string 
-TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "ATUAAv4AAQEAAQwpb8km5Nm22cdTLQFu9_ZSIWUevOgIYPdRuXCWhopVrbVZdVTYYLcrKpYZ6GOjCubqJ5M8z2O2yTEFuNvi0sWBtYiLJ5M7h02te82gBrXdfkmrT73P0AzRvxx1iSWYhm8e9QZY2uDg2fM72PPeVfmaToqzURLwb2V9LJN1m4WA1ClhIIzuKptbmCrW4sSy95nuNwH33uzoextJ1YbBeP5mBMjx1U56wutUNdYFt8Wd7VXYfuehptGHtFQg_9pRJELygAABGmF19lC_MnBkS5VGK4_e3iKAj4k-IEVtDEWYCahVqqpN9ASpXk2ClDF0WQdKoXuZxMOa2J6okH-AgBh8hv1bAQE2YfPGAQAD")
+TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
 
 # ID of Channel from which the bot shoul search files
 MAINCHANNEL_ID = os.environ.get("MAINCHANNEL_ID", "-1002346089650")
